23fea4f3dfe726fe35fa7b69c8fc79670b138800
Nishad <nshah49@gatech.edu>
2016-08-27 15:09:24 -0400

    Added the MTW class, initialize its variables based on fresh_tomatoes.py, added showTrailer method, and added at least one movie to make sure it's working properly.

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 import fresh_tomatoes
+import webbrowser
 
+###########################################
+#                 CLASS                   #
+###########################################
+#Setting up the class for MovieTrailerWebsite
+class MTW():
+    def __init__(self, title, storyline, poster, youtubetrailer):
+        #Initializes up title, storyline??, poster, youtubetrailer
+        self.title = title
+        self.storyline = storyline
+        self.poster_image_url = poster
+        self.trailer_youtube_url = youtubetrailer
+    def showTrailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+
+###########################################
+#            RETRIEVE MOVIES              #
+###########################################
+
+starwars7 = MTW("Star Wars Episode VII: The Force Awakens", "Return of the REAL Star Wars saga.",
+                "http://vignette2.wikia.nocookie.net/starwars/images/f/fd/Star_Wars_Episode_VII_The_Force_Awakens.jpg/revision/latest?cb=20151018162823",
+                "https://www.youtube.com/watch?v=sGbxmsDFVnE")
+mymovies = [starwars7]
+fresh_tomatoes.open_movies_page(mymovies)
